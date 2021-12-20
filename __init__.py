5276a2eb9e8e868947ef7d323d25fa28beb68f13
Amit Raj <amit.894@gmail.com>
2021-12-20 20:25:04 +0530

    Some additional features

--- __init__.py
+++ __init__.py
@@ -1,11 +1,24 @@
 from jira import JIRA
+import azure.functions as func
 
-username="amit.894@gmail.com"
-token="NotMyBitCoin"
-options = {
-    'server': 'https://amit894.atlassian.net/'}
-jira = JIRA(
-        basic_auth=(username,token),options=options)
 
-jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
-                              assignee={'name': 'amitraj'})
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    username="amit.894@gmail.com"
+    token="NotMyBitCoin"
+    options = {
+        'server': 'https://amit894.atlassian.net/'}
+    jira = JIRA(
+            basic_auth=(username,token),options=options)
+
+    jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
+                                  assignee={'name': 'amitraj'})
+    return func.HttpResponse(
+        json.dumps({
+            'method': req.method,
+            'url': req.url,
+            'headers': dict(req.headers),
+            'params': dict(req.params),
+            'get_body': req.get_body().decode()
+        })
+    )
