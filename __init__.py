79081295b8b943ee8598859600d5326f519d85a7
Amit Raj <amit.894@gmail.com>
2021-12-20 20:28:03 +0530

    Adding token for temp testing

--- __init__.py
+++ __init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
     username="amit.894@gmail.com"
-    token="NotMyBitCoin"
+    token="FNZSbBxpLmjAjw17Tnvs3E30"
     options = {
         'server': 'https://amit894.atlassian.net/'}
     jira = JIRA(
