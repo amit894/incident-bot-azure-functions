ad634289c73e07e23b41af8575e730e64b2b1bcb
Amit Raj <amit.894@gmail.com>
2021-12-21 07:45:32 +0530

    Test function

--- __init__.py
+++ __init__.py
@@ -1,20 +1,14 @@
-from jira import JIRA
+# from jira import JIRA
+# import azure.functions as func
+# from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
+
+
+import logging
+import json
 import azure.functions as func
-from azure.appconfiguration import AzureAppConfigurationClient, ConfigurationSetting
-
-
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    username="amit.894@gmail.com"
-    jira_token = os.getenv('AZURE_APP_CONFIG_JIRA_TOKEN')
-    options = {
-        'server': 'https://amit894.atlassian.net/'}
-    jira = JIRA(
-            basic_auth=(username,jira_token),options=options)
-
-    jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
-                                  assignee={'name': 'amitraj'})
     return func.HttpResponse(
         json.dumps({
             'method': req.method,
@@ -24,3 +18,24 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             'get_body': req.get_body().decode()
         })
     )
+
+# def main(req: func.HttpRequest) -> func.HttpResponse:
+#     logging.info('Python HTTP trigger function processed a request.')
+#     username="amit.894@gmail.com"
+#     jira_token = os.getenv('AZURE_APP_CONFIG_JIRA_TOKEN')
+#     options = {
+#         'server': 'https://amit894.atlassian.net/'}
+#     jira = JIRA(
+#             basic_auth=(username,jira_token),options=options)
+#
+#     jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
+#                                   assignee={'name': 'amitraj'})
+#     return func.HttpResponse(
+#         json.dumps({
+#             'method': req.method,
+#             'url': req.url,
+#             'headers': dict(req.headers),
+#             'params': dict(req.params),
+#             'get_body': req.get_body().decode()
+#         })
+#     )
