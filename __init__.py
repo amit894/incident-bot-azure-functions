f87183d4315a92555250b8ac7b0b6c6c50e97e6a
Amit Raj <amit.894@gmail.com>
2021-12-27 18:56:30 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,20 @@
 import logging
 import json
+from jira import JIRA 
 import azure.functions as func
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+    username="amit.894@gmail.com"
+    token="NotMyBitCoin"
+    options = {
+        'server': 'https://amit894.atlassian.net/'}
+    jira = JIRA(
+            basic_auth=(username,token),options=options)
+
+    jira.create_issue(summary="Third Issue", description="Yes this is JIRA", issuetype={'name': 'Task'}, project='MB',
+                                  assignee={'name': 'amitraj'})
     return func.HttpResponse(
         json.dumps({
             'method': req.method,
